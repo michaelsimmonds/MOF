a92e80249c887f711769abd1bd4fe42fa3ea4963
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-05-11 17:12:17 +0100

    A few more minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -55,7 +55,7 @@ class Home extends React.Component {
           }
         },
         {
-          breakpoint: 420,
+          breakpoint: 450,
           settings: {
             slidesToShow: 1,
             centerMode: false
