415cd39c5f146a97152326cfd9800a1664853ac5
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-05-11 12:32:11 +0100

    Added slight styling change to depth of border and made mobile responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,7 +38,33 @@ class Home extends React.Component {
       slidesToScroll: 1,
       swipeToSlide: true,
       nextArrow: <SampleNextArrow/>,
-      prevArrow: <SamplePrevArrow/>
+      prevArrow: <SamplePrevArrow/>,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false
+          }
+        }
+      ]
     }
 
     return (
